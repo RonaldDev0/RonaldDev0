1d57417713feb59f4a5585480fe9c4f372c05fa2
Ronald <ronaldzamoradev@gmail.com>
2025-02-27 16:02:12 -0500

    shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home () {
   return (
-    <main className='w-screen h-screen flex flex-col justify-center items-center'>
+    <main className='w-screen h-screen flex flex-col justify-center items-center gap-3'>
       <h1>Hello, world!</h1>
       <h2>This is my portfolio!!</h2>
+      <Button>Hello world!!</Button>
     </main>
   )
 }
