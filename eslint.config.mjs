22cb07daec417d8e9912f217978f43a3e8b4ec82
Ronald <ronaldzamoradev@gmail.com>
2025-02-27 11:41:20 -0500

    setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,50 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  baseDirectory: __dirname
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      'comma-dangle': ['error', 'never'],
+      semi: ['error', 'never'],
+      'react-hooks/exhaustive-deps': 'off',
+      'space-before-function-paren': ['error', {
+        anonymous: 'always',
+        named: 'always',
+        asyncArrow: 'always'
+      }],
+      'func-call-spacing': ['error', 'never'],
+      eqeqeq: ['error', 'always'],
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'object-curly-spacing': ['error', 'always'],
+      'array-bracket-spacing': ['error', 'never'],
+      indent: ['error', 2],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-loop-func': 'error',
+      'no-eval': 'error',
+      'prefer-template': 'error',
+      'no-dupe-keys': 'error',
+      'no-duplicate-case': 'error',
+      'no-multi-spaces': 'error',
+      'no-trailing-spaces': 'error',
+      'space-infix-ops': 'error',
+      'keyword-spacing': 'error',
+      'comma-spacing': ['error', { before: false, after: true }],
+      quotes: ['error', 'single'],
+      'jsx-quotes': ['error', 'prefer-single'],
+      '@typescript-eslint/no-empty-object-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'off'
+    }
+  }
+]
 
-export default eslintConfig;
+export default eslintConfig
