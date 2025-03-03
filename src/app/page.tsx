c6527c81359648524f72a17a88f5a68e85536454
Ronald <ronaldzamoradev@gmail.com>
2025-03-02 22:02:28 -0500

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from '@/components/sections/Hero'
 import Experience from '@/components/sections/Experience'
 import Projects from '@/components/sections/Projects'
 import Stack from '@/components/sections/Stack'
-import Education from '@/components/sections/Education'
+import Education from '@/components/sections/Educations'
 import AboutMe from '@/components/sections/About_me'
 
 export default function Home () {
