78df9ad7f57cf5a83d11af440d6b0c89893ed761
Ronald <ronaldzamoradev@gmail.com>
2025-03-02 22:01:14 -0500

    test

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -83,7 +83,7 @@ export default function Projects () {
                   </CardContent>
                   <CardFooter>
                     <CardDescription className='hover:text-white transition-colors px-1 py-3 w-full'>
-                      View Project
+                      View Projects
                     </CardDescription>
                   </CardFooter>
                 </Card>
