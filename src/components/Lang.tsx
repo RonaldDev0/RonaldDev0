3fae0c308cf5af37a88b1f83920017986e9a6ae6
Ronald <ronaldzamoradev@gmail.com>
2025-03-04 13:39:19 -0500

    fix: lang component responsive

--- src/components/Lang.tsx
+++ src/components/Lang.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Lang ({ en }: { en: boolean }) {
   return (
-    <div className='absolute top-4 right-4 flex gap-2 items-center justify-center text-xl font-bold'>
+    <div className='absolute min-[1000px]:top-4 max-[1000px]:-top-12 right-4 flex gap-2 items-center justify-center text-xl font-bold'>
       <Link href='/en' className={`hover:scale-125 transition-transform p-2 ${en && 'text-primary'}`}>
         En
       </Link>
